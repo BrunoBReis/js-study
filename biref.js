22050c87992f968e8edd864e9997529597ccf3e1
BrunoBReis <brunobragancadosreis@gmail.com>
2024-10-29 15:19:51 -0300

    feat: simple statement

---
+++ biref.js
@@ -0,0 +1,19 @@
+// Simple if statement
+
+let number = 10;
+if (number % 2 == 0) {
+    console.log(`Number ${number} is/are even`);
+}
+else {
+    console.log(`Number ${number} is/are odd`);
+}
+
+const test = 1 + 2 + "2";
+const anotherTest = "2" + 1 + 2;
+const x = Math.pow(2,2);
+const anotherValue = (100 + 50) * 3;
+
+console.log(test)
+console.log(anotherTest)
+console.log(x)
+console.log(anotherValue)
