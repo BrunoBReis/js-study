3fd16f2de23741c54d9b7d8ef9d632b9dd28a385
BrunoBReis <brunobragancadosreis@gmail.com>
2024-10-29 15:20:14 -0300

    feat: function

---
+++ function.js
@@ -0,0 +1,12 @@
+function sum(a, b){
+    return a + b;
+}
+
+let simpleFunction = sum(5,10)
+
+console.log(sum(2, 3))
+console.log(sum(2))
+console.log(sum())
+console.log(sum)
+console.log(simpleFunction)
+
